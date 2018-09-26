eb0c784e75895f7cf718e21e9d3d1660610af4a4
Filipe Natanael <filipenatanael@live.com>
2018-09-25 22:17:18 -0300

    Starter-files-added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 import React, { Component } from 'react';
 
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map((person) => (
-        <li key={person.name}>{ person.name }</li>
-      ))}
-    </ol>
-  }
-}
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Steve Rogers' },
-          { name: 'Maria Hill' },
-          { name: 'Nick Fury' }
-        ]}
-        />
-
-        <ContactList contacts={[
-          { name: 'Maria Hill' },
-          { name: 'Adaga' },
-          { name: 'Elektra' }
-        ]}
-        />
+      <div>
+        Hello World
       </div>
-    );
+    )
   }
 }
 
 export default App;
+
+
+// import React, { Component } from 'react';
+//
+// class ContactList extends Component {
+//   render() {
+//     const people = this.props.contacts
+//
+//     return <ol>
+//       {people.map((person) => (
+//         <li key={person.name}>{ person.name }</li>
+//       ))}
+//     </ol>
+//   }
+// }
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <ContactList contacts={[
+//           { name: 'Steve Rogers' },
+//           { name: 'Maria Hill' },
+//           { name: 'Nick Fury' }
+//         ]}
+//         />
+//
+//         <ContactList contacts={[
+//           { name: 'Maria Hill' },
+//           { name: 'Adaga' },
+//           { name: 'Elektra' }
+//         ]}
+//         />
+//       </div>
+//     );
+//   }
+// }
+//
+// export default App;
