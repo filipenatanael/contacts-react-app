e1f4e3e12559e06b826926d05285e01c38f42b46
Filipe Natanael <filipenatanael@live.com>
2018-10-01 23:56:54 -0300

    Add Contact Details

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react'
 
 class ListContacts extends Component{
   render() {
-    const people = this.props.contacts
+    const contacts = this.props.contacts
     return (
       <ol className='contact-list'>
-        {people.map((person) => (
-          <li key={person.id}>{person.name}</li>
+        {contacts.map((contact) => (
+          <li key={contact.id} className='contact-list-item'>
+            <div
+              className='contact-avatar'
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}></div>
+
+              <div className='contact-details'>
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+
+              <button className='contact-remove'>
+                Remove
+              </button>
+          </li>
         ))}
       </ol>
     )
   }
 }
-
 
 export default ListContacts
