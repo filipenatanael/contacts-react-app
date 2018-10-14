c39b1903b7fffab1bc0fa460812bd6011b345800
Filipe Natanael <filipenatanael@live.com>
2018-10-14 16:47:37 -0300

    Feat: Handle Submission Of The Contact Form

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import serializeForm from 'form-serialize';
 import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
   handleSubmit = (event) => {
     event.preventDefault()
+    const values = serializeForm(event.target, { hash: true })
+
+    if (this.props.onCreateContact) {
+      this.props.onCreateContact(values)
+    }
   }
+
   render() {
     return (
       <div>
