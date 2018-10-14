138f2450e638816c1e89cbe55f4aa9a6a83654ca
Filipe Natanael <filipenatanael@live.com>
2018-10-14 16:35:07 -0300

    Feat: Install form-serialize

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault()
+  }
   render() {
     return (
       <div>
@@ -11,7 +14,7 @@ class CreateContact extends Component {
         Close
         </Link>
 
-        <form className='create-contact-form'>
+        <form onSubmit={this.handleSubmit} className='create-contact-form'>
           <ImageInput className='create-contact-avatar-input'
           name='avatarURL'
           maxHeight={64}
